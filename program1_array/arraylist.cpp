936f739d2bfae9c6fb4ded58848f248634e15dc4
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2026-04-05 18:42:56 +0800

    Implement ResidentArray custom array class

---
+++ program1_array/arraylist.cpp
@@ -0,0 +1,46 @@
+#include "arraylist.h"
+
+// Constructor: allocate array
+ResidentArray::ResidentArray(int cap) {
+    capacity = cap;
+    count = 0;
+    data = new Resident[capacity];
+}
+
+// Deconstructor: free up memory
+ResidentArray::~ResidentArray() {
+    delete[] data;
+}
+
+// Add resident to end
+void ResidentArray::add(const Resident& r) {
+    if (count < capacity)
+        data[count++] = r;
+}
+
+// Get by index (non-const)
+Resident& ResidentArray::get(int index) {
+    return data[index];
+}
+
+// Get by index (const)
+const Resident& ResidentArray::get(int index) const {
+    return data[index];
+}
+
+// Swap two elements
+void ResidentArray::swap(int i, int j) {
+    Resident temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+}
+
+// Return count
+int ResidentArray::size() const {
+    return count;
+}
+
+// Reset count
+void ResidentArray::clear() {
+    count = 0;
+}
